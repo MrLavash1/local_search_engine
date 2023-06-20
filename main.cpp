73a45e7ed0352c906b78f5f6e333021975606dc3
MrLavash1 <136970825+MrLavash1@users.noreply.github.com>
2023-06-20 19:08:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
  * @details This file contains the configuration options for Doxygen.
  * It is used to generate documentation for the localSearchEngine project.
  *
- * @author
+ * @author Mark
  *
  */
 
@@ -20,41 +20,132 @@
 #include <QJsonArray>
 
 #include <iostream>
+#include <string>
+
 
 #include "header/ConvertJSON.h"
 #include "header/InvertedIndex.h"
 #include "header/SearchEngine.h"
+#include <QLineEdit>
+#include <QFileDialog>
+
 
 int main(int argc, char *argv[])
 {
     tm t;
     QApplication app(argc, argv);
+    QString fileName;
 
     QWidget window;
+    window.resize(600, 800);
     QVBoxLayout layout(&window);
+
+    QLineEdit lineEdit;
+    layout.addWidget(&lineEdit);
+    QPushButton browsButton("Browse...");
+    layout.addWidget(&browsButton);
+
+
+    QLabel label("Информация о Движке");
+    layout.insertWidget(2, &label);
+
 
     QTextEdit textEdit1;
     layout.addWidget(&textEdit1);
 
     QTextEdit textEdit2;
     layout.addWidget(&textEdit2);
-
-    QPushButton button("Старт");
-    layout.addWidget(&button);
-
-
-    try
-    {
-        ConverterJSON data;
-        InvertedIndex index;
-        index.UpdateDocumentBase(data.GetTextDocuments());
-
-        SearchServer search(index);
-        data.PutAnswers(search.Search(data.GetRequests(), data.GetResponsesLimit()));
+    QLabel label2("Результат движка");
+    layout.insertWidget(4, &label2);
 
 
-        QObject::connect(&button, &QPushButton::clicked, [&]() {
-            QFile file1("../json/config.json"); // замените на имя вашего файла
+    QPushButton startButton("Старт");
+    layout.addWidget(&startButton);
+    QPushButton closeButton("Закрыть");
+    layout.addWidget(&closeButton);
+
+    QObject::connect(&browsButton, &QPushButton::clicked, [&]() {
+        textEdit1.clear();
+        textEdit2.clear();
+        fileName = QFileDialog::getOpenFileName(&window, "Открыть файл конфигурации", "", "Файлы JSON (*.json)");
+        if (!fileName.isEmpty()) {
+            lineEdit.setText(fileName);
+        }
+
+
+    });
+
+
+    QObject::connect(&startButton, &QPushButton::clicked, [&]() {
+
+        try
+        {
+            ConverterJSON data;
+            InvertedIndex index;
+
+            index.UpdateDocumentBase(data.GetTextDocuments(fileName.toStdString()));
+
+            SearchServer search(index);
+            data.PutAnswers(search.Search(data.GetRequests(), data.GetResponsesLimit()));
+
+
+
+
+        }
+        catch (const ConfigFileEmpty_Exception& ex)
+        {
+            textEdit1.setPlainText(ex.what());
+
+        }
+        catch (const ConfigFileMissing_Exception& ex)
+        {
+            textEdit1.setPlainText(ex.what());
+
+        }
+        catch (const ConfigFileVersionEmpty_Exception& ex)
+        {
+            textEdit1.setPlainText(ex.what());
+
+        }
+        catch (const ConfigFileVersionIncorrect_Exception& ex)
+        {
+            textEdit1.setPlainText(ex.what());
+
+        }
+        catch (const RequestsFileEmpty_Exception& ex)
+        {
+            textEdit1.setPlainText(ex.what());
+
+        }
+        catch (const RequestsFileMissing_Exception& ex)
+        {
+            textEdit1.setPlainText(ex.what());
+
+        }
+        catch (const Text_ExceptionALotOf& ex)
+        {
+            textEdit1.setPlainText(ex.what());
+
+        }
+        catch (const Word_Exception100Chars& ex)
+        {
+            textEdit1.setPlainText(ex.what());
+
+        }
+        catch (const MoreThen1000Requests_Exception& ex)
+        {
+            textEdit1.setPlainText(ex.what());
+
+        }
+        catch (const WordsRequests_Exception& ex)
+        {
+            textEdit1.setPlainText(ex.what());
+
+        }
+
+
+        QObject::connect(&startButton, &QPushButton::clicked, [&]() {
+            QFile file1(fileName); // замените на имя вашего файла
             if (file1.open(QIODevice::ReadOnly)) {
                 QByteArray data = file1.readAll();
                 QJsonDocument doc = QJsonDocument::fromJson(data);
@@ -87,61 +178,16 @@ int main(int argc, char *argv[])
                 textEdit2.setPlainText(text);
             }
         });
-
-    }
-    catch (const ConfigFileEmpty_Exception& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-    catch (const ConfigFileMissing_Exception& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-    catch (const ConfigFileVersionEmpty_Exception& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-    catch (const ConfigFileVersionIncorrect_Exception& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-    catch (const RequestsFileEmpty_Exception& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-    catch (const RequestsFileMissing_Exception& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-    catch (const Text_ExceptionALotOf& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-    catch (const Word_Exception100Chars& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-    catch (const MoreThen1000Requests_Exception& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-    catch (const WordsRequests_Exception& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
+    });
 
 
-
-
-
-
-
+    QObject::connect(&closeButton, &QPushButton::clicked, [&]() {
+        window.close();
+    });
 
 
     window.show();
 
     return app.exec();
 
-
-    return 0;
 }
